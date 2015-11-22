06107c637c53d2a93c6007dca19934f689d14895
tgoldenberg <thomasagoldenberg@gmail.com>
2015-11-21 19:59:16 -0500

    styled landing page

---
+++ startupWeekend/application/components/colors.js
@@ -0,0 +1,8 @@
+const Colors = {
+  aqua: '#69B5CF',
+  mediumBlue: '#4F7EAD',
+  stars: '#EBA63D',
+  bodyText: '#4A4A4A',
+}
+
+module.exports = Colors;
--- startupWeekend/application/components/landing.js
+++ startupWeekend/application/components/landing.js
@@ -1,21 +1,61 @@
 
 var React = require('react-native');
+import Colors from './colors';
 var {
   AppRegistry,
   StyleSheet,
   Text,
   View,
   NavigatorIOS,
+  StyleSheet,
+  TouchableHighlight,
 } = React;
 
 class Landing extends React.Component{
   render(){
     return (
-      <View style={{flex: 1, backgroundColor: 'red'}}>
-        <Text>THIS IS THE LANDING</Text>
+      <View style={{flex: 1,}}>
+        <View style={styles.container}>
+          <Text style={styles.title}>Welcome to Career Climb</Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableHighlight
+            style={styles.button}
+            underlayColor={Colors.mediumBlue}
+          >
+            <Text style={styles.buttonText}>Sign in with LinkedIn</Text>
+          </TouchableHighlight>
+        </View>
       </View>
+
     )
   }
 }
+
+let styles = StyleSheet.create({
+  buttonContainer: {
+    backgroundColor: Colors.mediumBlue,
+  },
+  button: {
+    padding: 30,
+  },
+  buttonText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    backgroundColor: Colors.aqua,
+  },
+  title: {
+    fontSize: 29,
+    color: 'black',
+    textAlign: 'center',
+  }
+})
 
 module.exports = Landing;
